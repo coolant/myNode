03f7c1d319277fa0cd86ca1fe0c66737548dfcf3
AYKUT YILMAZ <shopare2003@gmail.com>
2024-08-19 21:23:45 +0200

    added del put post get methodes for hero

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express") //import to express
 const app = express()
 
+app.get("/superheroes", (req, res) => {
+  res.send({ msg: "Showing all heroes" })
+})
+
+app.get("/superheroes/:superheroId", (req, res) => {
+  res.send({ msg: "Showing hero with id " + req.params.superheroId })
+})
+
 app.get("/", (req, res) => {
-  res.send("Hello world") //send hello world to website
+  res.send({ msg: "Api server is running" }) //send hello world to website
+})
+
+app.post("/superheroes", (req, res) => {
+  res.send({ msg: "Creating a hero" })
+})
+
+app.put("/superheroes/:superheroId", (req, res) => {
+  res.send({ msg: "Editing superhero " + req.params.superheroId })
+})
+
+app.delete("/superheroes/:superheroId", (req, res) => {
+  res.send({ msg: "Deleting superhero " + req.params.superheroId })
 })
 
 app.listen(3000, console.log("Server started on port 3000")) //listen 3000 port
